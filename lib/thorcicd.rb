0096e54fe565ce0ae085979b57eb736d9ef2e669
Jake Gentry <gentry.jake@outlook.com>
2016-05-18 19:58:45 -0500

    First commit

---
+++ lib/thorcicd.rb
@@ -0,0 +1,15 @@
+require 'thorcicd/version'
+require 'foodcritic'
+require 'rspec/core/rake_task'
+require 'rubocop/rake_task'
+require 'berkshelf'
+require 'github_changelog_generator/task'
+
+projectName = 'elastalert-cookbook'
+supermarketUrl = 'https://supermarket.chef.io'
+
+module Thorcicd
+
+  # Your code goes here...
+
+end
---
+++ lib/thorcicd/version.rb
@@ -0,0 +1,7 @@
+
+module Thorcicd
+
+  VERSION = "0.1.0"
+
+end
+
